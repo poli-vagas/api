c9f2d92905713841aeeedd50136c11ca29ea7745
Mario Simão <mariosimao@poli.ufrj.br>
2023-05-22 19:50:42 -0300

    Enable CORS

--- Program.cs
+++ Program.cs
@@ -6,14 +6,11 @@ using PoliVagas.Core.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var emailSettings = builder.Configuration.GetSection("EmailSettings").Get<EmailSettings>();
-if (emailSettings != null) builder.Services.AddSingleton(emailSettings);
-
 builder.Services.AddControllers()
                 .AddJsonOptions(o => {
                     o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(o => {
@@ -40,7 +37,20 @@ builder.Services.AddSwaggerGen(o => {
     o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
+// Cors settings
+var corsPolicy = "corsPolicy";
+builder.Services.AddCors(options => {
+    options.AddPolicy(corsPolicy, policy => {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Dependency Injection
+// Settings
+var emailSettings = builder.Configuration.GetSection("EmailSettings").Get<EmailSettings>();
+if (emailSettings != null) builder.Services.AddSingleton(emailSettings);
 // Handlers
 builder.Services.AddTransient<PoliVagas.Core.Application.RegisterJob.RegisterJobHandler>();
 builder.Services.AddTransient<PoliVagas.Core.Application.FindJob.FindJobHandler>();
@@ -72,6 +82,7 @@ app.UseSwaggerUI(c => {
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors(corsPolicy);
 
 app.MapControllers();
 
